7dc2a564fd04157eb0618111da71f70c8db067a0
BigJoon <hiyoungih@naver.com>
2022-05-10 15:10:52 +0900

    add ympe.py module file

--- tools/data/skeleton/yj_mmlab_pose_extraction.py
+++ tools/data/skeleton/yj_mmlab_pose_extraction.py
@@ -1,1 +1,4 @@
-#폴더 안에 있는 모든 클립 파일들에 대해서 포즈를 추출해서 지정한 경로로 ".pkl"파일을 만들어주는 코드 
+#폴더 안에 있는 모든 클립 파일들에 대해서 포즈를 추출해서 지정한 경로로 ".pkl"파일을 만들어주는 코드
+import subprocess
+
+subprocess.call("ntu_pose_extraction.py",shell=True)
